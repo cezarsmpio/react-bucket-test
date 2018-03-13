a955f92b488a441f08192326af74b8fb251d115c
Cezar Luiz <cezarluiz.c@gmail.com>
2018-03-13 15:37:25 +0100

    Drawing the final component api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
+// const GoogleAnalytics = {
+//   onLoad: (props) => {
+//       ga('send', 'event', {
+//           eventCategory: props.name,
+//       });
+//   },
+//   willMount: () => {
+
+//   },
+//   didMount: () => {
+
+//   },
+//   willUnmount: () => {
+
+//   },
+// };
+
+// <Test name="Homepage CTA" driver?={GoogleAnalytics}>
+//   <Control id?="Control" traffic?="20">
+//       <div>Control</div>
+//   </Control>
+//   <Variation id?="Control" traffic?="40">
+//       <div>Variation 1</div>
+//   </Variation>
+//   <Variation id?="Control 2" traffic?="40">
+//       <div>Variation 2</div>
+//   </Variation>
+// </Test>
+
+// --
+
+// Category
+// Action
+// Label
+// Event Value
+
 import React, {Component} from 'react'
 
 export default class extends Component {
